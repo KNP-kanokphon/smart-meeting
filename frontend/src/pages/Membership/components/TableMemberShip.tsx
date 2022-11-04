4566b6d1ca666b7dd847b854f7f1443f347eeafd
RVAS <wasmeej@osd.co.th>
2022-11-04 14:04:45 +0700

    signature and update package and edit modal

--- frontend/src/pages/Membership/components/TableMemberShip.tsx
+++ frontend/src/pages/Membership/components/TableMemberShip.tsx
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Row, Typography, Button, Col, Table, Select, Input } from 'antd';
+import {
+  Card,
+  Row,
+  Typography,
+  Button,
+  Col,
+  Table,
+  Select,
+  Input,
+  Modal,
+  Form,
+  Space,
+  Tooltip,
+} from 'antd';
 import type { TableRowSelection } from 'antd/es/table/interface';
 import { useNavigate } from 'react-router-dom';
 import { AlignRightOutlined, MoreOutlined } from '@ant-design/icons';
@@ -9,8 +22,26 @@ export const TableMemberShip: React.FC = (): React.ReactElement => {
   const { Search } = Input;
   const navigate = useNavigate();
   const [dataResult, setDataResult] = useState<any>([]);
-  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [modalVisible, setmodalVisible] = useState<boolean>(false);
+
+  const showModal = () => {
+    setmodalVisible(true);
+  };
+
+  const handleOpen = () => {
+    setmodalVisible(false);
+  };
+
+  const handleOK = () => {
+    setmodalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setmodalVisible(false);
+  };
+
+  // const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  // const [loading, setLoading] = useState(false);
 
   // interface DataType {
   //   key: React.Key;
@@ -20,32 +51,16 @@ export const TableMemberShip: React.FC = (): React.ReactElement => {
   //   lastname: string;
   //   phone: string;
   // }
-  const start = () => {
-    setLoading(true);
-    // ajax request after empty completing
-    setTimeout(() => {
-      setSelectedRowKeys([]);
-      setLoading(false);
-    }, 1000);
-  };
+  // const start = () => {
+  //   setLoading(true);
+  //   // ajax request after empty completing
+  //   setTimeout(() => {
+  //     setSelectedRowKeys([]);
+  //     setLoading(false);
+  //   }, 1000);
+  // };
 
   const onSearch = (value: string) => console.log(value);
-
-  useEffect(() => {
-    async function dataTable() {
-      let dataconvert: any = [];
-      await dataSourceToday.map((e: any) => {
-        dataconvert.push({
-          key: e.key,
-          id: e.id,
-          name: e.title + ' ' + e.firstname + ' ' + e.lastname,
-          phone: e.phone,
-        });
-      });
-      await setDataResult(dataconvert);
-    }
-    dataTable();
-  }, []);
 
   let dataSourceToday: any = [
     {
@@ -66,10 +81,26 @@ export const TableMemberShip: React.FC = (): React.ReactElement => {
     },
   ];
 
+  useEffect(() => {
+    async function dataTable() {
+      let dataconvert: any = [];
+      await dataSourceToday.map((e: any) => {
+        dataconvert.push({
+          key: e.key,
+          id: e.id,
+          name: e.title + ' ' + e.firstname + ' ' + e.lastname,
+          phone: e.phone,
+        });
+      });
+      await setDataResult(dataconvert);
+    }
+    dataTable();
+  }, []);
+
   const columnsToday: any = [
     {
       title: 'ลำดับที่',
-      dataIndex: 'no',
+      dataIndex: 'key',
       fixed: 'left',
     },
     {
@@ -97,57 +128,131 @@ export const TableMemberShip: React.FC = (): React.ReactElement => {
       render: (text: any, row: any) => {
         return (
           <div style={{ textAlign: 'center' }}>
-            <Button style={{ border: 'none' }}>
-              <MoreOutlined />
-            </Button>
+            <Tooltip title={'Edit'}>
+              <Button style={{ border: 'none' }} onClick={showModal}>
+                <MoreOutlined />
+              </Button>
+            </Tooltip>
           </div>
         );
       },
     },
   ];
 
-  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
-    setSelectedRowKeys(newSelectedRowKeys);
-  };
+  // const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+  //   console.log('selectedRowKeys changed: ', newSelectedRowKeys);
+  //   setSelectedRowKeys(newSelectedRowKeys);
+  // };
 
-  const rowSelection = {
-    selectedRowKeys,
-    onChange: onSelectChange,
-  };
+  // const rowSelection = {
+  //   selectedRowKeys,
+  //   onChange: onSelectChange,
+  // };
 
-  const hasSelected = selectedRowKeys.length > 0;
+  // const hasSelected = selectedRowKeys.length > 0;
   return (
-    <Card
-      style={{ width: '100%', textAlign: 'left', marginBottom: '30px' }}
-      title={
-        <Row gutter={16} style={{ textAlign: 'right' }}>
-          <Col span={4}>
-            <Typography
-              style={{
-                textAlign: 'left',
-                fontWeight: 'bold',
-              }}
-            >
-              รายชื่อคณะกรรมการกลางสมาคมแห่งสถาบันพระปกเกล้า
-            </Typography>
-          </Col>
-          <Col span={20}>
-            <Select placeholder={'Filter'} bordered={false}>
-              <Option>Filter</Option>
+    <>
+      <Modal
+        title="แก้ไขข้อมูล"
+        open={modalVisible}
+        footer={null}
+        onOk={handleOpen}
+        onCancel={handleCancel}
+      >
+        <Form layout="vertical">
+          <Form.Item label={'คำนำหน้า'}>
+            <Select placeholder="Text">
+              <Option key={0} value={'m'}>
+                นาย
+              </Option>
+              <Option key={1} value={'f'}>
+                นางสาว
+              </Option>
             </Select>
-            <Search
-              placeholder="input search text"
-              allowClear
-              onSearch={onSearch}
-              style={{ width: 'auto' }}
-            />
-          </Col>
-        </Row>
-      }
-    >
-      {/* Test */}
-      {/* <Button
+          </Form.Item>
+          <Form.Item label={'ชื่อ'}>
+            <Input placeholder={'Text'} />
+          </Form.Item>
+          <Form.Item label={'สกุล'}>
+            <Input placeholder={'Text'} />
+          </Form.Item>
+          <Form.Item label={'เบอร์โทรศัพท์'}>
+            <Input placeholder={'Text'} />
+          </Form.Item>
+          <Form.Item label={'หลักสูตร'}>
+            <Select placeholder="Please Select" allowClear>
+              <Option key={0} value={'manager'}>
+                นายกสมาคม
+              </Option>
+              <Option key={1} value={'assistant_manager'}>
+                นายกกิติมศักดิ์และประธานฝ่ายกิจกรรมสาธารณะประโยชน์
+              </Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label={'ตำแหน่ง'}>
+            <Select placeholder="Please Select" allowClear>
+              <Option key={0} value={'aec1'}>
+                AEC.10
+              </Option>
+              <Option key={1} value={'tag10'}>
+                ที.เอ.จี.10
+              </Option>
+              <Option key={2} value={'tag1'}>
+                ที.เอ.จี.1
+              </Option>
+              <Option key={1} value={'tag2'}>
+                ที.เอ.จี.2
+              </Option>
+              <Option key={1} value={'tag3'}>
+                ที.เอ.จี.3
+              </Option>
+            </Select>
+          </Form.Item>
+          <Form.Item>
+            <div style={{ textAlign: 'right' }}>
+              <Space>
+                <Button onClick={handleCancel}>ยกเลิก</Button>
+                <Button
+                  onClick={handleOK}
+                  style={{ background: '#1E6541', color: 'white' }}
+                >
+                  ยืนยัน
+                </Button>
+              </Space>
+            </div>
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Card
+        style={{ width: '100%', textAlign: 'left', marginBottom: '30px' }}
+        title={
+          <Row gutter={16} style={{ textAlign: 'right' }}>
+            <Col span={4}>
+              <Typography
+                style={{
+                  textAlign: 'left',
+                  fontWeight: 'bold',
+                }}
+              >
+                รายชื่อคณะกรรมการกลางสมาคมแห่งสถาบันพระปกเกล้า
+              </Typography>
+            </Col>
+            <Col span={20}>
+              <Select placeholder={'Filter'} bordered={false}>
+                <Option>Filter</Option>
+              </Select>
+              <Search
+                placeholder="input search text"
+                allowClear
+                onSearch={onSearch}
+                style={{ width: 'auto' }}
+              />
+            </Col>
+          </Row>
+        }
+      >
+        {/* Test */}
+        {/* <Button
         type="primary"
         onClick={start}
         disabled={!hasSelected}
@@ -158,13 +263,14 @@ export const TableMemberShip: React.FC = (): React.ReactElement => {
       <span style={{ marginLeft: 8 }}>
         {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
       </span> */}
-      <Table
-        size="large"
-        rowSelection={rowSelection}
-        dataSource={dataResult}
-        columns={columnsToday}
-        // scroll={{ x: 'calc(600px + 50%)' }}
-      />
-    </Card>
+        <Table
+          size="large"
+          // rowSelection={rowSelection}
+          dataSource={dataResult}
+          columns={columnsToday}
+          // scroll={{ x: 'calc(600px + 50%)' }}
+        />
+      </Card>
+    </>
   );
 };
