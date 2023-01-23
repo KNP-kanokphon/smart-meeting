3d69e300a9eb12f73b3b155047a1e305988cb208
RVAS <wasmeej@osd.co.th>
2023-01-23 12:11:42 +0700

    css ui page

--- frontend/src/pages/voteagendes/loginvote.tsx
+++ frontend/src/pages/voteagendes/loginvote.tsx
@@ -9,12 +9,14 @@ import {
   Form,
   message,
   Typography,
+  Layout,
 } from 'antd';
 import Logo from '../../assets/images/KPIS Logo.png';
 import { useNavigate, useParams } from 'react-router-dom';
 import { DatamanagementService } from '../../stores/meeting-store';
+import styles from './MainLayout.module.scss';
 const { Text, Link } = Typography;
-
+const { Content, Sider, Header, Footer } = Layout;
 interface Idetailagenda {
   uuid: string;
   agendes: string;
@@ -61,99 +63,112 @@ export const Loginvote: React.FC<{}> = ({}): React.ReactElement => {
     }
   };
   return (
-    <>
-      <div
-        style={{
-          height: '100vh',
-          padding: '40px',
-          textAlign: 'center',
-          justifyContent: 'center',
-          display: 'flex',
-          backgroundColor: '#1E6541',
-        }}
-      >
-        <Row gutter={16}>
-          <Col xs={24} sm={24} md={24} lg={24}>
-            <Card
-              style={{
-                width: 'auto',
-                height: 'auto',
-                borderRadius: '25px 25px 25px 25px',
-                border: 'none',
-                marginTop: '90px',
-                boxShadow: 'rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px',
-              }}
-            >
+    // <>
+    <div
+      style={{
+        backgroundColor: '#1E6541',
+        height: 'auto',
+        width: '100%',
+        minHeight: '105vh',
+      }}
+    >
+      <Row gutter={16} style={{ justifyContent: 'center' }}>
+        <Col
+          xs={24}
+          sm={24}
+          md={24}
+          lg={12}
+          style={{
+            justifyContent: 'center',
+            display: 'flex',
+            height: '100%',
+            width: '100%',
+            marginTop: '80px',
+          }}
+        >
+          <Card
+            style={{
+              width: '80%',
+              borderRadius: '25px 25px 25px 25px',
+              border: 'none',
+              boxShadow: 'rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px',
+            }}
+          >
+            <div style={{ textAlign: 'center' }}>
               <Image
                 preview={false}
                 src={Logo}
-                style={{ width: '35%', height: '10%', marginBottom: '20px' }}
-              />
-              <div style={{ paddingLeft: '20px', paddingRight: '20px' }}>
-                <Form
-                  name="login"
-                  layout="vertical"
-                  autoComplete="off"
-                  onFinish={onFinish}
-                  style={{ textAlign: 'left' }}
-                >
-                  <Text>{detailagenda?.agendes}</Text>
-                  <Typography.Title level={5} style={{ margin: 0 }}>
-                    {detailagenda?.detailagendes}
-                  </Typography.Title>
-                  <br></br>
-                  <Form.Item
-                    label="เบอรโทรศัพท์"
-                    name="phonenumber"
-                    rules={[
-                      {
-                        required: true,
-                        message: 'กรุณากรอกเบอรโทรศัพท์!',
-                      },
-                    ]}
-                  >
-                    <Input
-                      placeholder="0XX-XXX-XXXX"
-                      style={{ width: '100%', borderRadius: '10px' }}
-                      size={'middle'}
-                      onChange={(event: any) =>
-                        setPhonenumber(event.target.value)
-                      }
-                    />
-                  </Form.Item>
-
-                  <Form.Item>
-                    <Button
-                      style={{
-                        background: '#1E6541',
-                        border: 'none',
-                        width: '100%',
-                        // borderRadius: '10px',
-                      }}
-                      size={'middle'}
-                      type="primary"
-                      htmlType="submit"
-                    >
-                      ถัดไป
-                    </Button>
-                  </Form.Item>
-                </Form>
-              </div>
-            </Card>
-            <div>
-              <div
                 style={{
-                  marginTop: '80px',
-                  textAlign: 'center',
-                  color: 'white',
+                  width: '35%',
+                  height: '10%',
+                  marginBottom: '20px',
                 }}
-              >
-                ©2022 O S D Company Limited
-              </div>
+              />
             </div>
-          </Col>
-        </Row>
-      </div>
-    </>
+            <div style={{ paddingLeft: '10%', paddingRight: '10%' }}>
+              <Form
+                name="login"
+                layout="vertical"
+                autoComplete="off"
+                onFinish={onFinish}
+                style={{ textAlign: 'left' }}
+              >
+                <Text>{detailagenda?.agendes}</Text>
+                <Typography.Title level={5} style={{ margin: 0 }}>
+                  {detailagenda?.detailagendes}
+                </Typography.Title>
+                <br></br>
+                <Form.Item
+                  label="เบอรโทรศัพท์"
+                  name="phonenumber"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'กรุณากรอกเบอรโทรศัพท์!',
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="0XX-XXX-XXXX"
+                    style={{ width: '100%', borderRadius: '10px' }}
+                    size={'middle'}
+                    onChange={(event: any) =>
+                      setPhonenumber(event.target.value)
+                    }
+                  />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    style={{
+                      background: '#1E6541',
+                      border: 'none',
+                      width: '100%',
+                      borderRadius: '10px',
+                    }}
+                    size={'middle'}
+                    type="primary"
+                    htmlType="submit"
+                  >
+                    ถัดไป
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+          </Card>
+        </Col>
+        <Col
+          span={24}
+          style={{
+            textAlign: 'center',
+            color: 'white',
+            marginTop: '60px',
+          }}
+        >
+          ©2022 O S D Company Limited
+        </Col>
+      </Row>
+    </div>
+    // </>
   );
 };
